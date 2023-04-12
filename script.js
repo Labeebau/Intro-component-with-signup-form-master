ea9e5203632be538a709eb0f99284debe1e3a130
Labeeba <77605700+Labeebau@users.noreply.github.com>
2023-04-12 13:21:50 +0530

    Added validation

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-document.getElementById("claim").addEventListener("click",function(){
-    var firstName=document.getElementById("first-name");
-    if(firstName.innerHTML=="")
-        document.querySelector("first-error").innerHTML="First Name cannot be empty";
+input = document.querySelectorAll(".inputs");
+errorImage = document.querySelectorAll(".error-icon");
+errorElement = document.querySelectorAll(".error-message");
+document.getElementById("claim").addEventListener("click", function () {
+    for (i = 0; i <= input.length; i++) {
 
+        if (input[i].value === "") {
+            input[i].style.border = "1px solid red";
+            errorImage[i].classList.add("visible");
+            errorElement[i].classList.add("visible");
+        }
+        else {
+            input[i].style.border = "";
+            errorImage[i].classList.remove("visible");
+            errorElement[i].classList.remove("visible");
+        }
+    }
 
 });
